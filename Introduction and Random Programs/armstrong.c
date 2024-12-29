00d49380203a4645a10c713541b7b756bd43cf3d
Piyush Varman (PCV) <varmanpiyush@gmail.com>
2024-12-29 18:32:32 +0530

    Update armstrong.c

--- Introduction and Random Programs/armstrong.c
+++ Introduction and Random Programs/armstrong.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int x, og, s=0, n, i;
-    printf("Enter the number to be checked for:");
+    int x,og,z,s=0,i=0;
+    printf("Enter a 3 digit number: ");
     scanf("%d",&x);
     og=x;
-    for (i=0;i<3;i++){
-        n=x%10;
+    while (x>0){
+        z=x%10;
+        s+=z*z*z;
         x=x/10;
-        s+=(n*n*n);
     }
     if (s==og){
-        printf("The number %d is an armstrong number.",og);
+        printf("The number %d is an Armstrong number.",og);
     }
-    else {
-        printf("The number %d is not an armstrong number.",og);
+    else{
+        printf("The number %d is NOT an Armstrong number.",og);
     }
     return 0;
 }
